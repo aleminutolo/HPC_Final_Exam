570ff9c8b487e8fc99e005a98cd40c697b035047
alesminutolo <ales.minutolo@studenti.unina.it>
2024-11-20 20:53:08 +0100

    m

---
+++ Ex1/sbatches/gather3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=HPC
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=128
+#SBATCH --time=01:59:58
+#SBATCH --partition EPYC
+#SBATCH --exclusive
+
+module load openMPI/4.1.6/gnu/14.2.1
+
+echo "Processes,Size,Latency" > gather3.csv
+
+# #interazions
+repetitions=10000
+
+for power in {1..8}
+do
+	processes=$((2**power))
+	for size_power in {1..18}
+	do
+		size=$((2**size_power))
+		result_gather=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_gather_algorithm 3 ../../osu_gather -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
+		
+		echo "$processes,$size,$result_gather"
+		echo "$processes,$size,$result_gather" >> gather3.csv
+	done
+done	
---
+++ Ex1/sbatches/gather3_fixed_size.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=HPC
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=128
+#SBATCH --time=02:00:00
+#SBATCH --partition EPYC
+#SBATCH --exclusive
+
+
+module load openMPI/4.1.6/gnu/14.2.1
+
+echo "Processes,Size,Latency" > gather3_fixed_core.csv
+
+# Numero di ripetizioni per ottenere una media
+repetitions=10000
+
+# Ciclo esterno per il numero di processori
+for processes in {2..256}
+do
+    # Calcola la dimensione come 2 elevato alla potenza corrente
+    size=4
+
+    # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
+    result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_gather_algorithm 3 ../../osu_gather -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
+
+    echo "$processes, $size, $result_bcast"
+    # Scrivi i risultati nel file CSV
+    echo "$processes,$size,$result_bcast" >> gather3_fixed_core.csv
+
+done
