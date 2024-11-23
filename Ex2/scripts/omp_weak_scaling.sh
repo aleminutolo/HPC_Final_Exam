7193d6ad857eaba25ff9b0e28525157923994396
Alessandro Minutolo <98023987+aleminutolo@users.noreply.github.com>
2024-11-23 17:25:07 +0100

    Update omp_weak_scaling.sh

--- Ex2/scripts/omp_weak_scaling.sh
+++ Ex2/scripts/omp_weak_scaling.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=HPC-OMP-Scaling
-#SBATCH --nodes=1                
-#SBATCH --ntasks=1                
-#SBATCH --cpus-per-task=128       
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
 #SBATCH --time=02:00:00
 #SBATCH --partition=EPYC
 #SBATCH --exclusive
- 
-mpicc -fopenmp mandelbrot.c -o mandelbrot -lm
+
 module load openMPI/4.1.6/gnu/14.2.1
+mpicc -fopenmp mandelbrot.c -o mandelbrot -lm
+
 
 # Output file
 OUTPUT_FILE="../csvs/omp_weak_scaling.csv"
@@ -23,14 +24,14 @@ MAX_ITERATIONS=255
 C=1000000
 
 for THREADS in {1..128}; do
-    
+
     export OMP_NUM_THREADS=$THREADS
     n=$(echo "scale=2; sqrt($THREADS * $C)" | bc)
     export OMP_PLACES=threads
     export OMP_PROC_BIND=true
 
     # Execution with just 1 process MPI (bind to/map by socket is necessary)
-    EXEC_TIME=$(mpirun -np 1 ./mandelbrot --map-by socket --bind-to socket $n $n $X_LEFT $Y_LOWER $X_RIGHT $Y_UPPER $MAX_ITERATIONS $THREADS)
+    EXEC_TIME=$(mpirun -np 1 --map-by socket ./mandelbrot $n $n $X_LEFT $Y_LOWER $X_RIGHT $Y_UPPER $MAX_ITERATIONS $THREADS)
 
     # Savings
     echo "1,$THREADS,$n,$n,$EXEC_TIME" >> $OUTPUT_FILE
